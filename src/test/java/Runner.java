55dccdf7d49f9dfb0ffd1f7477dbce49dd19615c
Jairo Andree Maida Rojas <jairotrabaja123@gmail.com>
2024-05-27 07:54:54 -0400

    commit inicial

---
+++ src/test/java/Runner.java
@@ -0,0 +1,12 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/java/features",
+    glue = {"step_definitions", "utility"},
+    plugin = {"pretty", "html:target/cucumber-html-report", "json:cucumber.json"}
+)
+public class Runner {
+}
