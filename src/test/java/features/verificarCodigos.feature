d844e63011dd192be7c16dbb568e74b5840b5a21
Winsor <winsoromar12@gmail.com>
2024-06-18 01:06:16 -0400

    pasos: todos los pasos terminados, arreglar feature

--- src/test/java/features/verificarCodigos.feature
+++ src/test/java/features/verificarCodigos.feature
@@ -2,19 +2,19 @@
 # date: 01/05/2024
 # language: es
 
-Requisito: Verificar que los códigos de registro se envíen al correo electrónico de un usuario
+Requisito: Verificar que los códigos de registro no se envíen al correo electrónico de un usuario ya registrado en el sistema
 
   Este test case verifica que los códigos de registro solicitados durante el proceso
   de registro se envíen al correo electrónico del usuario
 
-  Escenario: Registrar usuario
+  Escenario: Registrar usuario cliente
 
-  Esquema del escenario: Un usuario registrándose en la aplicación web
+  Esquema del escenario: Un usuario cliente registrándose en la aplicación web
     Dado Un usuario cliente registrándose en "https://friender.vercel.app/registrar"
-    Cuando Llena parcialmente el formulario con los datos "<Nombre>", "<Apellido paterno>", "<Apellido materno>", "<Fecha de nacimiento>", "<Género>", "<Ubicación>", "<Nombre de usuario>", "<Contraseña>", "<Confirmar contraseña>"
-    Y Pulsa el botón "Siguiente"
-    Y Pulsa el botón "Enviar Código"
-    Entonces Recibe códigos de verificación en la bandeja de su correo electrónico
+    Cuando Llena parcialmente el formulario con los datos "<Nombre>", "<Apellido paterno>", "<Apellido materno>", "<Fecha de nacimiento>", "<Genero>", "<Ubicacion>", "<Nombre de usuario>", "<Correo>", "<Password>"
+    Y Pulsa el botón 'Siguiente'
+    Y Ingresa un "<Codigo>" aleatorio
+    Entonces Recibe códigos de verificación en la bandeja de su "<Correo>" en "https://gmail.com/" usando su "<Password>"
     Ejemplos:
-      | Nombre | Apellido paterno | Apellido materno | Fecha de nacimiento | Género | Ubicación | Nombre de usuario | Contraseña | Confirmar contraseña |
-
+      | Nombre | Apellido paterno | Apellido materno | Fecha de nacimiento | Genero | Codigo | Ubicacion  | Nombre de usuario | Correo                 | Password  |
+      | Winsor | Orellana         | Vásquez          | 30012002            | M      | JHKEO  | Cochabamba | Winsor2002        | winsoromar12@gmail.com | Pepit@123 |
--- src/test/java/step_definitions/VerificarCodigos.java
+++ src/test/java/step_definitions/VerificarCodigos.java
@@ -4,21 +4,88 @@ import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.es.Entonces;
 import io.cucumber.java.es.Y;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 public class VerificarCodigos {
+    private WebDriver driver;
+
     @Dado("Un usuario cliente registrándose en {string}")
-    public void unUsuarioClienteRegistrandoseEn(String arg0) {
+    public void unUsuarioClienteRegistrandoseEn(String url) {
+        System.setProperty("webdriver.chrome.driver", "src/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+
+        driver = new ChromeDriver(options);
+        driver.get(url);
     }
 
     @Cuando("Llena parcialmente el formulario con los datos {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}")
-    public void llenaParcialmenteElFormularioConLosDatos(String arg0, String arg1, String arg2, String arg3, String arg4, String arg5, String arg6, String arg7, String arg8, String arg9) {
+    public void llenaParcialmenteElFormularioConLosDatos(String nombre, String apPaterno, String apMaterno,
+                                                         String fechaNacimiento, String genero, String ubicacion,
+                                                         String nomUsuario, String correo, String password) {
+        Map<String, String> fieldData = new HashMap<>();
+        fieldData.put("nombre", nombre);
+        fieldData.put("apellido_paterno", apPaterno);
+        fieldData.put("apellido_materno", apMaterno);
+        fieldData.put("fecha_nacimiento", fechaNacimiento);
+        fieldData.put("genero", genero);
+        fieldData.put("ubicacion", ubicacion);
+        fieldData.put("nombre_usuario", nomUsuario);
+        fieldData.put("correo_electronico", correo);
+        fieldData.put("contraseña", password);
+
+        for (Map.Entry<String, String> entry : fieldData.entrySet()) {
+            WebElement input = new WebDriverWait(driver, Duration.ofSeconds(10))
+                    .until(ExpectedConditions.visibilityOfElementLocated(By.id(entry.getKey())));
+            input.sendKeys(entry.getValue());
+            try {
+                Thread.sleep(700);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        driver.findElement(By.id("confirmar_contraseña")).sendKeys(password);
     }
 
-    @Y("Pulsa el botón {string}")
-    public void pulsaElBoton(String arg0) {
+
+    @Y("Pulsa el botón 'Siguiente'")
+    public void pulsaElBotonSiguiente() {
+        driver.findElement(By.xpath("//button[contains(@class, 'false')]")).click();
     }
 
-    @Entonces("Recibe códigos de verificación en la bandeja de su correo electrónico")
-    public void recibeCodigosDeVerificacionEnLaBandejaDeSuCorreoElectronico() {
+    @Y("Ingresa un {string} aleatorio")
+    public void pulsaElBotonEnviarCodigo(String codigo) {
+        WebElement button = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[contains(@placeholder, 'Codigo')]")));
+        button.sendKeys(codigo);
+    }
+
+    @Entonces("Recibe códigos de verificación en la bandeja de su {string} en {string} usando su {string}")
+    public void recibeCodigosDeVerificacionEnLaBandejaDeSuEnUsandoSu(String email, String urlGmail, String password) {
+        driver.findElement(By.xpath("//button[contains(@class, 'btn-verde')]")).click();
+        WebElement message = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/div[2]/div/p[6]")));
+
+        Assert.assertEquals(message.getText(), "Código de verificación incorrecto.");
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.close();
     }
 }
